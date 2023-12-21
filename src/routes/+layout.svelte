517f4adaa8f004fcb7ede232be208f49df2430f2
Sandeep Singh <serperior2@protonmail.com>
2023-12-21 17:51:42 -0500

    add non mobile nav, mobile nav logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
+    import navigation from "$lib/_data/navigation.json";
     import CartPreview from "$lib/comp/cart-preview.svelte";
     import { onMount } from "svelte";
-    import navigation from "$lib/_data/navigation.json";
     import "../app.pcss";
     
     export let data;
@@ -17,6 +17,14 @@
     function toggleMobileDropdown() {
         navMobileDropdown?.classList.toggle('hidden')
     }
+
+    // collapse mobile dropdown when clicking outside of dropdown
+    // @ts-ignore
+    function handleDropdownFocusLoss({ relatedTarget, currentTarget }) {
+        // use "focusout" event to ensure that we can close the dropdown when clicking outside or when we leave the dropdown with the "Tab" button
+        if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return // check if the new focus target doesn't present in the dropdown tree (exclude ul\li padding area because relatedTarget, in this case, will be null
+        navMobileDropdown?.classList.add('hidden');
+    }
 </script>
 
 <svelte:head>
@@ -24,9 +32,9 @@
 </svelte:head>
 
 <div class="relative">
-    <nav class="fixed top-0 left-0 w-full bg-primary-200">
-        <!-- mobile -->
-        <div class="md:hidden flex justify-between bg-inherit">
+    <nav class="fixed top-0 left-0 w-full">
+        <!-- mobile nav -->
+        <div class="md:hidden flex justify-between bg-primary-200" on:focusout={handleDropdownFocusLoss}>
             <div><a href="/">Home</a></div>
 
             <!-- hamburger -->
@@ -37,7 +45,18 @@
             <!-- mobile -->
             <div class="hidden absolute top-0 left-0 w-full mt-6 flex flex-col bg-inherit text-center text-lg" id="nav-mobile-dropdown">
                 {#each navItems as item}
-                    <a href="{ item.url }" on:click={toggleMobileDropdown}>{ item.name }</a>
+                    <a href="{ item.url }" class="block">{ item.name }</a>
+                {/each}
+            </div>
+        </div>
+
+        <!-- non mobile nav -->
+        <div class="hidden md:flex justify-between">
+            <div><a href="/">Home</a></div>
+
+            <div>
+                {#each navItems as item}
+                    <a href="{ item.url }" class="inline-block py-2 px-3">{ item.name }</a>
                 {/each}
             </div>
         </div>
