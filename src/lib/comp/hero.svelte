8ecb30a18e534270a7d1c57d6a7a7921318a542e
Sandeep Singh <serperior2@protonmail.com>
2023-12-21 13:38:18 -0500

    add Hero and initial color scheme

---
+++ src/lib/comp/hero.svelte
@@ -0,0 +1,16 @@
+<div class="min-h-screen bg-primary-300 flex flex-wrap items-center p-4">
+    <!-- image -->
+    <div class="w-full md:w-1/2 my-8 md:mx-auto">
+        <img src="https://placehold.co/500" alt="" class="w-full max-w-[255px] max-h-[255px] md:max-w-[300px] md:max-h-[300px] lg:max-w-[375px] lg:max-h-[375px] mx-auto">
+    </div>
+
+    <!-- hero text -->
+    <div class="w-fit md:w-1/2">
+        <div class="w-fit mx-8 text-center md:text-left">
+            <h1 class="mb-2 text-5xl lg:text-6xl">Yummy Pasteries!</h1>
+            <p class="mb-4">Handbaked by yours truely, these are one of a kind!</p>
+    
+            <a href="/bakery" class="inline-block py-3 px-4 bg-primary-600 hover:bg-primary-500 text-white rounded-sm">Browse the bakery!</a>
+        </div>
+    </div>
+</div>
