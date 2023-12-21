42fadb231dcb211cb2b98185ca5e0a477edc0abb
Sandeep Singh <serperior2@protonmail.com>
2023-12-21 15:00:13 -0500

    add basic mobile navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,51 @@
 <script>
-  import CartPreview from "$lib/comp/cart-preview.svelte";
-import "../app.pcss";
-  
-  export let data;
+    import CartPreview from "$lib/comp/cart-preview.svelte";
+    import { onMount } from "svelte";
+    import navigation from "$lib/_data/navigation.json";
+    import "../app.pcss";
+    
+    export let data;
+    export const navItems = navigation;
+
+    /** @type {HTMLElement | null} */
+    export let navMobileDropdown;
+
+    onMount(() => {
+        navMobileDropdown = document.getElementById('nav-mobile-dropdown');
+    });
+
+    function toggleMobileDropdown() {
+        navMobileDropdown?.classList.toggle('hidden')
+    }
 </script>
 
 <svelte:head>
-  <script type="text/javascript" src="https://{data.squareCdnPrefix}web.squarecdn.com/v1/square.js"></script>
+    <script type="text/javascript" src="https://{data.squareCdnPrefix}web.squarecdn.com/v1/square.js"></script>
 </svelte:head>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/bakery">Bakery</a>
-</nav>
+<div class="relative">
+    <nav class="fixed top-0 left-0 w-full bg-primary-200">
+        <!-- mobile -->
+        <div class="md:hidden flex justify-between bg-inherit">
+            <div><a href="/">Home</a></div>
+
+            <!-- hamburger -->
+            <div>
+                <button type="button" id="nav-mobile-hamburger" on:click={toggleMobileDropdown}>X</button>
+            </div>
+
+            <!-- mobile -->
+            <div class="hidden absolute top-0 left-0 w-full mt-6 flex flex-col bg-inherit text-center text-lg" id="nav-mobile-dropdown">
+                {#each navItems as item}
+                    <a href="{ item.url }" on:click={toggleMobileDropdown}>{ item.name }</a>
+                {/each}
+            </div>
+        </div>
+    </nav>
+</div>
 
 <div>
-  <CartPreview
-    cart={data.locals.cart}
-  />
+    <CartPreview cart={data.locals.cart} />
 </div>
 
 <slot />
