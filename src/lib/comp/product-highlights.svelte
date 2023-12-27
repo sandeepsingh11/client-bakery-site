1fd9d00bbed89e0932306cb18099e9755651b059
Sandeep Singh <serperior2@protonmail.com>
2023-12-27 17:51:01 -0500

    style product card

--- src/lib/comp/product-highlights.svelte
+++ src/lib/comp/product-highlights.svelte
@@ -1,4 +1,7 @@
 <script>
+    import Button from './button.svelte';
+    import Card from './card.svelte';
+
     /** @type {import('$lib/_data/pages/home.json')} */
     export let _data;
     /** @type {App.Locals['products']} */
@@ -21,21 +24,7 @@
 
     <div class="flex flex-wrap justify-center">
         {#each matchedProducts as item}
-            <div class="w-[250px] m-4 border">
-                <!-- product image -->
-                <div class="w-full h-[250px]">
-                    <img src="{ (item.images?.at(0)) ? item.images[0].url : "https://placehold.co/500" }" alt="{ item.name }" class="w-full h-full object-cover">
-                </div>
-
-                <!-- product name and desc -->
-                <div>
-                    <h3 class="mb-1 font-medium">{ item.name }</h3>
-                    <p>${ item.displayPrice }</p>
-                    <p>{ item.desc }</p>
-                </div>
-
-                <a href="/bakery/{ item.slug }">View pastry</a>
-            </div>
+            <Card item={item} />
         {/each}
     </div>
 </section>
