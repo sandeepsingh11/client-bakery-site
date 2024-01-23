0acd3a5f567f75539dd9ae4a3c64a8997d1d9c60
Sandeep Singh <serperior2@protonmail.com>
2024-01-23 17:36:58 -0500

    start about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<section class="py-4 md:py-12">
+    <!-- first snippet -->
+    <div class="grid grid-cols-1 md:grid-cols-2 items-center mx-auto">
+        <div>
+            <p class="leading-8 md:py-8 md:px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque euismod vulputate. Nunc nec odio sed tellus sagittis dictum. Proin nec gravida lectus. Maecenas a augue sit amet sapien suscipit accumsan et et nisi. Ut molestie leo non diam bibendum, vel semper turpis suscipit. Cras vel nisl in neque dignissim consequat. Nullam non volutpat magna, pretium tempor odio. Vivamus rhoncus magna non est laoreet facilisis.</p>
+        </div>
+        <div>
+            <img src="https://placehold.co/500x900" alt="" class="w-full max-w-[300px] max-h-[300px] lg:max-w-[375px] lg:max-h-[375px] object-cover my-8 mx-auto rounded-full">
+        </div>
+    </div>
+
+    <!-- second snippet -->
+    <div class="grid grid-cols-1 md:grid-cols-2 items-center mx-auto">
+        <div>
+            <img src="https://placehold.co/500x900" alt="" class="w-full max-w-[300px] max-h-[300px] lg:max-w-[375px] lg:max-h-[375px] object-cover my-8 mx-auto rounded-full">
+        </div>
+        <div>
+            <p class="leading-8 md:py-8 md:px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque euismod vulputate. Nunc nec odio sed tellus sagittis dictum. Proin nec gravida lectus. Maecenas a augue sit amet sapien suscipit accumsan et et nisi. Ut molestie leo non diam bibendum, vel semper turpis suscipit. Cras vel nisl in neque dignissim consequat. Nullam non volutpat magna, pretium tempor odio. Vivamus rhoncus magna non est laoreet facilisis.</p>
+        </div>
+    </div>
+</section>
+
+<style>
+    section > div {
+        width: min(90%, 1080px);
+    }
+</style>
