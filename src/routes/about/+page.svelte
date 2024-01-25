d4aca445b630fdbf7cfbf0e132fcf58166b4fab3
Sandeep Singh <serperior2@protonmail.com>
2024-01-25 15:58:21 -0500

    cms about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,16 @@
+<script>
+    import _about from "$lib/_data/pages/about.json";
+</script>
+
 <section class="py-4 md:py-12">
     <!-- first snippet -->
     <div class="grid-container">
         <div class="grid grid-cols-1 md:grid-cols-2 items-center mx-auto">
             <div>
-                <p class="leading-8 md:py-8 md:px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque euismod vulputate. Nunc nec odio sed tellus sagittis dictum. Proin nec gravida lectus. Maecenas a augue sit amet sapien suscipit accumsan et et nisi. Ut molestie leo non diam bibendum, vel semper turpis suscipit. Cras vel nisl in neque dignissim consequat. Nullam non volutpat magna, pretium tempor odio. Vivamus rhoncus magna non est laoreet facilisis.</p>
+                <p class="leading-8 md:py-8 md:px-4">{ _about.aboutSection1 }</p>
             </div>
             <div>
-                <img src="https://placehold.co/500x900" alt="" class="w-full max-w-[300px] max-h-[300px] lg:max-w-[375px] lg:max-h-[375px] object-cover my-8 mx-auto rounded-full">
+                <img src="{ _about.aboutSection1Img }" alt="{ _about.aboutSection1ImgAlt }" class="w-full max-w-[300px] max-h-[300px] lg:max-w-[375px] lg:max-h-[375px] object-cover my-8 mx-auto rounded-full">
             </div>
         </div>
     </div>
@@ -15,11 +19,11 @@
     <div class="bg-primary-400 grid-container">
         <div class="grid grid-cols-1 md:grid-cols-2 items-center mx-auto">
             <div>
-                <img src="https://placehold.co/500x900" alt="" class="w-full max-w-[150px] max-h-[150px] md:max-w-[200px] md:max-h-[200px] lg:max-w-[225px] lg:max-h-[225px] xl:max-w-[250px] xl:max-h-[250px] object-cover mx-auto rounded-full -translate-x-[30%] translate-y-[15%]">
-                <img src="https://placehold.co/500x900" alt="" class="w-full max-w-[150px] max-h-[150px] md:max-w-[200px] md:max-h-[200px] lg:max-w-[225px] lg:max-h-[225px] xl:max-w-[250px] xl:max-h-[250px] object-cover mx-auto rounded-full translate-x-[30%] -translate-y-[15%]">
+                <img src="{ _about.aboutSection2Img1 }" alt="{ _about.aboutSection2Img1Alt }" class="w-full max-w-[150px] max-h-[150px] md:max-w-[200px] md:max-h-[200px] lg:max-w-[225px] lg:max-h-[225px] xl:max-w-[250px] xl:max-h-[250px] object-cover mx-auto rounded-full -translate-x-[30%] translate-y-[15%]">
+                <img src="{ _about.aboutSection2Img2 }" alt="{ _about.aboutSection2Img2Alt }" class="w-full max-w-[150px] max-h-[150px] md:max-w-[200px] md:max-h-[200px] lg:max-w-[225px] lg:max-h-[225px] xl:max-w-[250px] xl:max-h-[250px] object-cover mx-auto rounded-full translate-x-[30%] -translate-y-[15%]">
             </div>
             <div>
-                <p class="leading-8 md:py-8 md:px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque euismod vulputate. Nunc nec odio sed tellus sagittis dictum. Proin nec gravida lectus. Maecenas a augue sit amet sapien suscipit accumsan et et nisi. Ut molestie leo non diam bibendum, vel semper turpis suscipit. Cras vel nisl in neque dignissim consequat. Nullam non volutpat magna, pretium tempor odio. Vivamus rhoncus magna non est laoreet facilisis.</p>
+                <p class="leading-8 md:py-8 md:px-4">{ _about.aboutSection2 }</p>
             </div>
         </div>
     </div>
@@ -34,5 +38,9 @@
 
     .grid-container > div {
         width: min(90%, 1080px);
+    }
+
+    p {
+        white-space: pre-line;
     }
 </style>
